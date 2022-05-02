68c64e3ce92436b4168e0ef00f8b5278231693be
Alessandra <alessandra.destefano@mail.polimi.it>
2022-05-02 16:11:57 +0200

    product and cart

--- src/main/java/it/polimi/db2/servlets/CartServlet.java
+++ src/main/java/it/polimi/db2/servlets/CartServlet.java
@@ -1,13 +1,13 @@
-package it.polimi.db2.servlets.employee;
+package it.polimi.db2.servlets;
 
-import it.polimi.db2.entities.BundleEntity;
 import it.polimi.db2.entities.UserEntity;
+import it.polimi.db2.utils.Product;
 import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,11 +15,14 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "EmployeeHomepageServlet", value = "/employee/homepage")
-public class HomepageServlet extends HttpServlet {
+@WebServlet(name = "CartServlet", value = "/cart")
+public class CartServlet extends HttpServlet{
+
     private TemplateEngine templateEngine;
 
-    public void init() {
+    @Override
+    public void init() throws ServletException {
+
         ServletContext servletContext = getServletContext();
         ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
         templateResolver.setTemplateMode(TemplateMode.HTML);
@@ -29,18 +32,17 @@ public class HomepageServlet extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.setContentType("text/html");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect(getServletContext().getContextPath());
+    }
 
-        ServletContext servletContext = getServletContext();
-        WebContext ctx = new WebContext(req, resp, servletContext, req.getLocale());
-        String path = "/WEB-INF/employee/homepage.html";
-
-        templateEngine.process(path, ctx, resp.getWriter());
-
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
         UserEntity user = (UserEntity) session.getAttribute("user");
+        Product bundle = (Product) session.getAttribute("bundle");
+        Product validityPeriod = (Product) session.getAttribute("validityPeriod");
+        Product service = (Product) session.getAttribute("service");
 
-        BundleEntity bundle;
     }
 }
